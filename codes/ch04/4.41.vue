914a465339ee1b1829289a58754c28056e0ca805
africalib <africalibrary21@gmail.com>
2024-12-19 11:38:10 +0900

    코드 수정

--- codes/ch04/4.41.vue
+++ codes/ch04/4.41.vue
@@ -1,15 +1,3 @@
-<template>
-  <!-- 제출 시 login 메서드 호출(새로고침을 막기 위한 prevent) -->
-  <form @submit.prevent="login">
-    <!-- v-model을 활용한 양방향 데이터 바인딩 -->
-    <input type="text" v-model="state.form.id" placeholder="아이디"/>
-    <input type="password" v-model="state.form.pw" placeholder="패스워드"/>
-
-    <!-- 제출을 위한 버튼 -->
-    <button type="submit">로그인</button>
-  </form>
-</template>
-
 <script setup>
 import {reactive} from "vue";
 
@@ -28,3 +16,15 @@ const login = () => {
   window.alert(state.form.id + "님 환영합니다!");
 };
 </script>
+
+<template>
+  <!-- 제출 시 login 메서드 호출(새로고침을 막기 위한 prevent) -->
+  <form @submit.prevent="login">
+    <!-- v-model을 활용한 양방향 데이터 바인딩 -->
+    <input type="text" v-model="state.form.id" placeholder="아이디"/>
+    <input type="password" v-model="state.form.pw" placeholder="패스워드"/>
+
+    <!-- 제출을 위한 버튼 -->
+    <button type="submit">로그인</button>
+  </form>
+</template>
